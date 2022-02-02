d029e282d37ad9e1fe4dc49745bb7f625a7c4ac3
Matt Loberg <loberg.matt@gmail.com>
2022-02-01 20:53:07 -0600

    build(npm): add prettier
    Add prettier with Tailwind plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,17 @@
-const Encore = require("@symfony/webpack-encore");
+const Encore = require('@symfony/webpack-encore');
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-process.env.NODE_ENV = Encore.isProduction() ? "production": "dev";
+process.env.NODE_ENV = Encore.isProduction() ? 'production' : 'dev';
 
-Encore
-    .setOutputPath("assets/")
-    .setPublicPath("/assets")
-    .addStyleEntry("css/app", "./_assets/css/app.css")
-    .addEntry("js/price", "./_assets/js/price.js")
-    .enablePostCssLoader()
-
-    .disableSingleRuntimeChunk()
-
-    // .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    // .enableVersioning(Encore.isProduction())
-;
+Encore.setOutputPath('assets/')
+  .setPublicPath('/assets')
+  .addStyleEntry('css/app', './_assets/css/app.css')
+  .addEntry('js/price', './_assets/js/price.js')
+  .enablePostCssLoader()
+  .disableSingleRuntimeChunk()
+  .enableSourceMaps(!Encore.isProduction());
 
 module.exports = Encore.getWebpackConfig();
